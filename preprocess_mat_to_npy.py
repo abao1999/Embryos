bb79223cb9b9628ac34538b76be0f38e4a826762
Ethan Mann <e.milomann@gmail.com>
2020-10-21 21:20:04 -0500

    added polarization npy and documentation

--- preprocess_mat_to_npy.py
+++ preprocess_mat_to_npy.py
@@ -3,18 +3,27 @@ import h5py
 import numpy as np
 from notebooks import utils
 
+# Source directory of the raw *.mat files.
 bf_data_path = 'data/video_bf_data'
 fluo_data_path = 'data/video_fluo_data'
 
-bf_processed_path = 'processed/bf_data'
-fluo_processed_path = 'processed/fluo_data'
+# Output directory of the processed *.npy files
+processed_path = 'processed'
+bf_processed_path = f'{processed_path}/bf_data'
+fluo_processed_path = f'{processed_path}/fluo_data'
+polar_processed_path = f'{processed_path}/polarization'
 
+# Pairs of processing functions the corrresponding sub directory names
 funcs = [('middle', utils.get_middle_z), ('max', utils.max_across_z),
          ('min', utils.min_across_z), ('avg', utils.avg_across_z)]
 
+# Indices to process
 embryo_idx_arr = [47, 49, 50, 52, 53]
 
 def load_bf_video(embryo_idx):
+    '''
+    Loads the video and polarization data for bf @ embryo_idx
+    '''
     bf = h5py.File(os.path.join(bf_data_path,'embryo_'+str(embryo_idx)+'.mat'))
     arrays = {}
     for k, v in bf.items():
@@ -24,6 +33,9 @@ def load_bf_video(embryo_idx):
     return bf_video, pol_state
 
 def load_fluo_video(embryo_idx):
+    '''
+    Loads the video and polarization data for fluo @ embryo_idx
+    '''
     fluo = h5py.File(os.path.join(fluo_data_path,'embryo_'+str(embryo_idx)+'.mat'))
     arrays = {}
     for k, v in fluo.items():
@@ -32,7 +44,7 @@ def load_fluo_video(embryo_idx):
     pol_state = arrays['anno']
     return fluo_video, pol_state
 
-def process_and_save(embryo_idx, video, pol_state, top_path):
+def process_and_save(embryo_idx, video, top_path):
     '''
     Applies each type of preprocessing to the video for idx and saves as npy.
     '''
@@ -42,8 +54,9 @@ def process_and_save(embryo_idx, video, pol_state, top_path):
 
 if __name__ == '__main__':
     # Make all directories that do not yet exist.
-    if not os.path.isdir('processed'):
-        os.mkdir('processed')
+    for top_path in [processed_path, polar_processed_path]:
+        if not os.path.isdir(top_path):
+            os.mkdir(top_path)
     for top_path in [bf_processed_path, fluo_processed_path]:
         if not os.path.isdir(top_path):
             os.mkdir(top_path)
@@ -55,7 +68,9 @@ if __name__ == '__main__':
     # Process each idx with each method in funcs
     # Note: polarization data is currently not used
     for idx in embryo_idx_arr:
-        bf_video, pol_state = load_bf_video(idx)
-        process_and_save(idx, bf_video, pol_state, bf_processed_path)
-        fluo_video, pol_state = load_fluo_video(idx)
-        process_and_save(idx, fluo_video, pol_state, fluo_processed_path)
+        bf_video, bf_pol_state = load_bf_video(idx)
+        process_and_save(idx, bf_video, bf_processed_path)
+        fluo_video, fluo_pol_state = load_fluo_video(idx)
+        process_and_save(idx, fluo_video, fluo_processed_path)
+        assert (bf_pol_state == fluo_pol_state).all()
+        np.save(f'{polar_processed_path}/embryo_{idx}', bf_pol_state)
